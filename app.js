d08ae724916eee22417a42de5f385cc4d5daf93a
bilkn <65674342+bilkn@users.noreply.github.com>
2020-12-15 22:08:06 +0300

    feat: add babel

--- app.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-let test = 'hello';
-let test2 = "hello2";
-console.log(test);
-console.log(test2);
-console.log("this is for feature branch.");
---
+++ src/main.js
@@ -0,0 +1,8 @@
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+}
+
+console.log(new Person("john", 25));
